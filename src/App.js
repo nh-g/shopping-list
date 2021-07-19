dda86d9236bb2aa2285c3f03041c4875e900daf7
nh-g <giangngohong@gmail.com>
2021-07-19 23:27:27 +0200

    add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
 
+import "./styles/styles.sass";
+import Navigation from "./components/Navigation";
 function App() {
   return (
     <div className="App">
-      <h1>EIKA shopping list</h1>
-
+      <BrowserRouter>
+        <Navigation />
+        <h1>EIKA shopping list</h1>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+//NPM Packages
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+//Project files
+import logo from '../assets/images/logo.svg'
+export default function Navigation() {
+    return (
+    <nav id="navigation-bar">
+        <Link to="/" >
+            <img src={logo} alt="logo" className="logo"/>
+        </Link>
+
+    </nav>
+);
+}
+
