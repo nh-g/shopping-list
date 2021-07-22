a863a5574851e67176719775a54e2015be473b68
nh-g <giangngohong@gmail.com>
2021-07-23 00:19:23 +0200

    add basic function of create form, display list components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./styles/styles.sass";
 import Navigation from "./components/Navigation";
+import TodoList from './components/TodoList'
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
+    <BrowserRouter>
+      <Navigation />
+      <div className="todo-app">
         <h1>EIKA shopping list</h1>
-      </BrowserRouter>
-    </div>
+        <TodoList />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react'
+import Todo from './Todo'
+import TodoForm from './TodoForm'
+
+export default function TodoList() {
+    const [todos, setTodos] = useState([])
+
+
+    const addTodo = todo => {
+        // Type-in verification
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+        const newTodos = [todo, ...todos]
+        setTodos(newTodos)
+    }
+    
+    const updateTodo = (todoId, newValue) => {
+      // Type-in verification
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+          return;
+        }
+        setTodos(prev => prev.map((todo) => (todo.id === todoId? newValue : todo )))
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos)
+    }
+
+    const removeTodo = id => {
+        const afterRemoveArray= [...todos].filter(todo => todo.id !== id) 
+        setTodos(afterRemoveArray);
+    }
+
+    const clearCompleted = () =>{
+        let cleared = [...todos].filter(todo => {
+            return !todo.isComplete
+        })
+        setTodos(cleared)
+    } 
+
+    const viewCompleted = () =>{
+        let completedList = [...todos].filter(todo => {
+            return todo.isComplete
+        })
+        setTodos(completedList);
+    }
+
+    useEffect(() => {
+      const todos = JSON.parse(localStorage.getItem("todos"));
+      if (todos) {
+        setTodos(todos);
+      }
+    }, []);
+
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+
+    return (
+      <div>
+        <TodoForm onSubmit={addTodo} />
+        <Todo
+          todos={todos}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          updateTodo={updateTodo}
+          clearCompleted={clearCompleted}
+          viewCompleted={viewCompleted}
+        />
+        <button onClick={clearCompleted} className="button">
+          Clear Completed
+        </button>
+        <button onClick={viewCompleted} className="button yellow">
+          View Completed
+        </button>
+      </div>
+    );
+}
