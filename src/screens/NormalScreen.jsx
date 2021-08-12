58d1c789e7f2517d88b6cee611dfc3ae42a8f77a
nh-g <giangngohong@gmail.com>
2021-08-12 03:38:20 +0200

    add functionality to show/hide the acquired list

--- src/screens/NormalScreen.jsx
+++ src/screens/NormalScreen.jsx
@@ -3,8 +3,17 @@ import CreateItemButton from "../components/CreateItemButton";
 import ShoppingList from "../components/ShoppingList";
 import SortControl from "../components/SortControl";
 import ItemForm from "../components/ItemForm";
+import CompletedList from "../components/CompletedList";
+import Item from "../components/Item";
+import AcquiredItem from "../components/AcquiredItem";
+import AcquiredList from "../components/AcquiredList";
 
-export default function NormalScreen({ list }) {
+export default function NormalScreen({ list, removeItem, completeItem }) {
+  const [showCompletedList, setShowCompletedList] = useState(false);
+  console.log("NormalScreen SHOW", { showCompletedList});
+  const toggleShow = () => setShowCompletedList(!showCompletedList);
+
+
   // const [shoppingList, setShoppingList] = useState(list);
   // console.log("NormalScreen state", { shoppingList, list });
   const inactiveList = list.filter((item) => item.isComplete === true);
@@ -12,8 +21,25 @@ export default function NormalScreen({ list }) {
 
   return (
     <div>
-      {/* <SortControl list={items} setList={setItems} /> */}
-      <ShoppingList shoppingListItems={activeList} />
+      {/* <SortControl list={list} /> */}
+      {/* active items */}
+      <ShoppingList
+        shoppingListItems={activeList}
+        removeItem={removeItem}
+        completeItem={completeItem}
+      />
+      {/* acquired items */}
+      <CompletedList
+        showCompleted={showCompletedList}
+        toggleShow={toggleShow}
+      />
+      {showCompletedList && (
+        <AcquiredList
+          shoppingListItems={inactiveList}
+          removeItem={removeItem}
+          completeItem={completeItem}
+        />
+      )}
     </div>
   );
 }
