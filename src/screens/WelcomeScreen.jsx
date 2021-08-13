5a3e81cd00875122c399a9b21403e004f5a3715f
nh-g <giangngohong@gmail.com>
2021-08-13 22:24:22 +0200

    add welcome screen

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -3,7 +3,7 @@ import Image from "../assets/images/shopping.jpg";
 export default function WelcomeScreen() {
   return (
     <div id="welcome-page">
-      <img src={Image} alt="Couple Shopping" />
+      <img src={Image} alt="Couple Shopping"  style={{width:"330px"}}/>
       <p>
         Welcome to EIKA’s shopping list. Here you will be able to create a todo
         list with for the products you want to buy.
@@ -11,7 +11,6 @@ export default function WelcomeScreen() {
       <p>
         To get started press the blue Add items button and a popup will ask you
         the name and the price and the quantity of the item you want to add. 
-        You can also and an image after the item is added by touching the camera icon.
       </p>
     </div>
   );
