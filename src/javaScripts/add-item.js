ec10e426986686e7fece545187a9f0271242f315
nh-g <giangngohong@gmail.com>
2021-08-30 14:27:07 +0200

    refactor: move addItem method outsite by creating separate js function

---
+++ src/javaScripts/add-item.js
@@ -0,0 +1,11 @@
+export const requestNewItem = (values) => {
+  const newItem = {
+    id: Math.floor(Math.random() * 100000),
+    productName: values.productName.toUpperCase(),
+    price: values.price,
+    quantity: values.quantity,
+    isComplete: false,
+  };
+
+  return newItem;
+};
