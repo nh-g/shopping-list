35053de9aaf654cbbf6c6f69ae1b09c2e9223b42
nh-g <giangngohong@gmail.com>
2021-09-30 16:41:49 +0200

    add test: welcome screen displays

---
+++ src/App.test.jsx
@@ -0,0 +1,24 @@
+// NPM packages
+import {render, screen} from "@testing-library/react";
+
+// Project files
+import App from "./App";
+
+test("Should show the welcome screen if the list is empty", () => {
+  // Arrange
+  const fakeData = [];
+
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(fakeData));
+
+  render(
+      <App />
+  );
+
+  // Act
+  const textElement = screen.getByText(/Welcome to EIKA’s shopping list/i);
+
+  // Assert
+  expect(textElement).toBeInTheDocument();
+
+});
+
