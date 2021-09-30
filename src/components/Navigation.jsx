bc9d60b88ad4d8f6fd18082452d76675d188d60f
nh-g <giangngohong@gmail.com>
2021-09-30 15:48:45 +0200

    remove Link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,12 @@
 //NPM Packages
 import React from "react";
-import { Link } from "react-router-dom";
 
 //Project files
 import logo from "../assets/images/logo.svg";
 export default function Navigation() {
   return (
     <nav id="navigation-bar">
-      {/* Removed unused comments. This is clearly a legacy of when you where using the Router */}
-      {/* <Link to="/" > */}
       <img src={logo} alt="logo" className="logo" />
-      {/* </Link> */}
     </nav>
   );
 }
