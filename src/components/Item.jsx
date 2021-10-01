14826bc11d0795eb55545e0c27a9173bbdae0b03
nh-g <giangngohong@gmail.com>
2021-10-01 15:46:35 +0200

    add tbody tag to fix bug: validateDOMNesting(...): <tr> cannot appear as a child of <table>

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -14,26 +14,33 @@ export default function Item({listItem, editList, removeItem}) {
   }
   
   return (
-    <tr
-      className={isComplete ? "shopping-item complete" : "shopping-item"}
-      key={id}
-    >
-      <td>
-        <Checkbox checked={isComplete} onChange={() => editItem("isComplete", !isComplete)}/>
-      </td>
-      <td className="name">{productName}</td>
-      <td className="quantity">
-        {quantity}
-        {quantity ? " pc" : ""}
-      </td>
-      <td className="price">{price}:-</td>
-      <td>
-        <RiDeleteBinLine
-          onClick={() => removeItem(id)}
-          className="delete-icon"
-          style={{ color: "salmon" }}
-        />
-      </td>
-    </tr>
+    <>
+      <tbody>
+        <tr
+          className={isComplete ? "shopping-item complete" : "shopping-item"}
+          key={id}
+        >
+          <td>
+            <Checkbox
+              checked={isComplete}
+              onChange={() => editItem("isComplete", !isComplete)}
+            />
+          </td>
+          <td className="name">{productName}</td>
+          <td className="quantity">
+            {quantity}
+            {quantity ? " pc" : ""}
+          </td>
+          <td className="price">{price}:-</td>
+          <td>
+            <RiDeleteBinLine
+              onClick={() => removeItem(id)}
+              className="delete-icon"
+              style={{ color: "salmon" }}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </>
   );
 }
