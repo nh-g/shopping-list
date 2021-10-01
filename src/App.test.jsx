2fe036d86a0140472adceaf76da1ac522ac2e8bd
nh-g <giangngohong@gmail.com>
2021-10-01 15:50:47 +0200

    added global mock by using jest.fn

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,12 +6,12 @@ import App from "./App";
 
 test("Should show the welcome screen if the list is empty", () => {
   // Arrange
-  const fakeData = [];
+  const mockData = [];
 
-  Storage.prototype.getItem = jest.fn(() => JSON.stringify(fakeData));
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(mockData));
 
   render(
-      <App />
+    <App />
   );
 
   // Act
@@ -22,3 +22,35 @@ test("Should show the welcome screen if the list is empty", () => {
 
 });
 
+test("The local storage key is null", () => {
+  // Arrange
+  const mockLocalStorageData = null;
+
+  // localStorage.getItem()
+  Storage.prototype.getItem = jest.fn(() => mockLocalStorageData); // null;
+
+  render(<App />);
+
+  // Act
+  const textElement = screen.getByText(/Welcome to EIKA’s shopping list/i);
+
+  // Assert
+  expect(textElement).toBeInTheDocument();
+});
+
+test("The local storage has information (return > 1 items)", () => {
+  // Arrange
+  const mockData = [
+    { id: 1, productName: "sofa", price: 9000, quantity: 1, isComplete: false },
+  ];
+
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(mockData));
+
+  render(<App />);
+
+  // Act
+  const item = screen.getByText(/sofa/i);
+
+  // Assert
+  expect(item).toBeInTheDocument();
+});
